6c736a59197d527d99d048c80de9a4a93baa3d9a
LSTK <l.stk@aol.com>
2023-04-19 22:01:58 +0100

    roulette finished

--- Lukas/Script4_SelectionProcess.py
+++ Lukas/Script4_SelectionProcess.py
@@ -1,5 +1,19 @@
-def roulette():
-    return
+from Script1_Data import nutrients, commodities, columns
+from Script3_Fitness import monetary_fitness
+import numpy as np
+
+def roulette(population, commodities):
+    # calculate fitness for all individuals in population
+    pop_fitness = monetary_fitness(population, commodities)
+    # calculate probability for each individual
+    pop_proba = pop_fitness / sum(pop_fitness)
+    # create list with range of population
+    pop_range = np.arange(len(population))
+    # select individual based on probability via pop_range
+    select = [np.random.choice(pop_range, p=pop_proba) for i in range(len(population))]
+    # final selection of individuals
+    select_final = [population[i] for i in select]
+    return select_final
 
 def ranked():
     return
