1cc6c4cf6483a653bf8e14cec0702807a4c2dc5f
LSTK <l.stk@aol.com>
2023-04-19 17:21:08 +0100

    rename scripts

--- Lukas/01_initial_population.py
+++
@@ -1,5 +0,0 @@
-from Data import nutrients, commodities
-import random
-
-def random_population(N):
-    return
---
+++ Lukas/Script2_InitialPopulation.py
@@ -0,0 +1,10 @@
+from Script1_Data import nutrients, commodities, columns
+import random
+
+#################
+# Test Parameters
+population_size = 10
+#################
+
+def random_population(population_size):
+    return
