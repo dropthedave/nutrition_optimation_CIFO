35cef1d87bfb7488891b4dc99fa30e7db42cc0fe
LSTK <l.stk@aol.com>
2023-04-21 19:28:07 +0100

    update

--- Lukas/Script3_Fitness.py
+++ Lukas/Script3_Fitness.py
@@ -8,20 +8,18 @@ def monetary_fitness(population, nutrients=nutrients, commodities=commodities):
     '''
     # create list with prices
     price_list = np.array([i[0] for i in np.array(list(commodities.values()))])
-
-    # create constraint list
+    # create constraints list
     constraints =np.array(list(nutrients.values()))
-
+    # create empty list for the fitness values
     fitness = np.array([])
-
+    # calculate fitness for each solution
     for solution in population:
         cumulative_nutrients = np.zeros(len(nutrients.keys()))
         for index, i in enumerate(solution):
             if i == 1:
                 cumulative_nutrients += np.array(list(commodities.values())[index][1:])
-        
+        # check if all constraints are met
         check = constraints - cumulative_nutrients
-
         if np.all(check <= 0):
             fitness = np.append(fitness, np.dot(solution, price_list))
         else:
--- Lukas/Script6_GeneticAlgorithm.py
+++ Lukas/Script6_GeneticAlgorithm.py
@@ -1,11 +1,11 @@
 from Script1_Data import nutrients, commodities
-from Script2_InitialPopulation import random_population
+from Script2_InitialPopulation import initial_population
 from Script3_Fitness import monetary_fitness
 from Script4_SelectionProcess import roulette
 # from Script5_Variation import mutation, crossover
 import numpy as np
 
-generations = 100
+generations = 200
 population_size = 100
 
 def genetic_algorithm(generations, population_size, nutrients=nutrients, commodities=commodities, mutation_rate=0.1, crossover_rate=0.8):
@@ -14,13 +14,13 @@ def genetic_algorithm(generations, population_size, nutrients=nutrients, commodi
 	fitness_progress = []
 
 	# create initial population of size n
-	population = random_population(population_size, nutrients, commodities)
+	population = initial_population(population_size, nutrients, commodities)
 
 	for gen in range(generations):
 		
 		# selection phase (based on fitness)
-		parents_population = roulette(population, commodities)
-		
+		parents_population = roulette(population, nutrients, commodities)
+
 		# apply variations (check for daily nutrients)
 		# offspring_population = variation(parents_population)
 	
@@ -29,9 +29,9 @@ def genetic_algorithm(generations, population_size, nutrients=nutrients, commodi
 		population = parents_population
 
 		# calculate minimum fitness of each population
-		print(min(monetary_fitness(parents_population, commodities)))
+		print(min(monetary_fitness(parents_population, nutrients, commodities)))
 		# fitness_progress.append(min(monetary_fitness(population, commodities)))
 	
-	return #fitness_progress
+	return -1 
 
 print(genetic_algorithm(generations, population_size))
